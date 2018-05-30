cb4d9e7f33db4d024ca2a16c50767a3534442ba6
Nicolas Marini <nmarini44@gmail.com>
2018-05-30 00:51:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+min_key = nil
+min_value = nil
+name_hash.each do |key, value|
+  if min_value == nil || value < min_value
+    key = min_key
+    value = min_value
+  end
+end 
+min_key
+  
 end
